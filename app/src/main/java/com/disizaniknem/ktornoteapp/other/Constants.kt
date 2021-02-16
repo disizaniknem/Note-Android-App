daba3c670c8360a8188c6391280e465480e3d26c
disizaniknem <disizaniknem@gmail.com>
2021-02-16 23:33:15 +0100

    Dagger Hilt setup

--- app/src/main/java/com/disizaniknem/ktornoteapp/other/Constants.kt
+++ app/src/main/java/com/disizaniknem/ktornoteapp/other/Constants.kt
@@ -4,4 +4,10 @@ object Constants {
 
     val IGNORE_AUTH_URLS = listOf("/login", "/register")
 
+    const val DATABASE_NAME = "notes_db"
+
+    const val BASE_URL = "http://192.168.1.72:8001"
+
+    const val ENCRYPTED_SHARED_PREF_NAME = "enc_shared_pref"
+
 }
