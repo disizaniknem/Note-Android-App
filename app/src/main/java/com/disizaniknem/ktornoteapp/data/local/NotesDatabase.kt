d1e1b70100fdaa695e9356f9c921e9ca3dcf874c
disizaniknem <disizaniknem@gmail.com>
2021-02-16 20:09:59 +0100

    Setting up room database

---
+++ app/src/main/java/com/disizaniknem/ktornoteapp/data/local/NotesDatabase.kt
@@ -0,0 +1,16 @@
+package com.disizaniknem.ktornoteapp.data.local
+
+import androidx.room.Database
+import androidx.room.TypeConverters
+import com.disizaniknem.ktornoteapp.data.local.entities.Note
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class NotesDatabase {
+
+    abstract fun noteDao(): NoteDao
+
+}
