7d4cbf57e7868b219e5c5f82eb52c447c8be820c
disizaniknem <disizaniknem@gmail.com>
2021-02-17 16:38:21 +0100

    Adding owner to notes

--- app/src/main/java/com/disizaniknem/ktornoteapp/repositories/NoteRepository.kt
+++ app/src/main/java/com/disizaniknem/ktornoteapp/repositories/NoteRepository.kt
@@ -6,6 +6,7 @@ import com.disizaniknem.ktornoteapp.data.local.entities.LocallyDeletedNoteId
 import com.disizaniknem.ktornoteapp.data.local.entities.Note
 import com.disizaniknem.ktornoteapp.data.remote.NoteApi
 import com.disizaniknem.ktornoteapp.data.remote.requests.AccountRequest
+import com.disizaniknem.ktornoteapp.data.remote.requests.AddOwnerRequest
 import com.disizaniknem.ktornoteapp.data.remote.requests.DeleteNoteRequest
 import com.disizaniknem.ktornoteapp.other.Resource
 import com.disizaniknem.ktornoteapp.other.checkForInternetConnection
@@ -98,6 +99,19 @@ class NoteRepository @Inject constructor(
                 checkForInternetConnection(context)
             }
         )
+    }
+
+    suspend fun addOwnerToNote(owner: String, noteId: String) = withContext(Dispatchers.IO) {
+        try {
+            val response = noteApi.addOwnerToNote(AddOwnerRequest(owner, noteId))
+            if (response.isSuccessful && response.body()!!.successful) {
+                Resource.success(response.body()?.message)
+            } else {
+                Resource.error(response.body()?.message ?: response.message(), null)
+            }
+        } catch (e: Exception) {
+            Resource.error("Couldn't connect to the servers. Check your internet connection", null)
+        }
     }
 
     suspend fun login(email: String, password: String) = withContext(Dispatchers.IO) {
--- app/src/main/java/com/disizaniknem/ktornoteapp/ui/notedetail/NoteDetailFragment.kt
+++ app/src/main/java/com/disizaniknem/ktornoteapp/ui/notedetail/NoteDetailFragment.kt
@@ -1,17 +1,22 @@
 package com.disizaniknem.ktornoteapp.ui.notedetail
 
 import android.os.Bundle
-import android.view.View
+import android.view.*
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.disizaniknem.ktornoteapp.R
 import com.disizaniknem.ktornoteapp.data.local.entities.Note
+import com.disizaniknem.ktornoteapp.other.Status
 import com.disizaniknem.ktornoteapp.ui.BaseFragment
+import com.disizaniknem.ktornoteapp.ui.dialogs.AddOwnerDialog
 import dagger.hilt.android.AndroidEntryPoint
 import io.noties.markwon.Markwon
+import kotlinx.android.synthetic.main.fragment_add_edit_note.*
 import kotlinx.android.synthetic.main.fragment_note_detail.*
+
+const val ADD_OWNER_DIALOG_TAG = "ADD_OWNER_DIALOG_TAG"
 
 @AndroidEntryPoint
 class NoteDetailFragment : BaseFragment(R.layout.fragment_note_detail) {
@@ -22,6 +27,15 @@ class NoteDetailFragment : BaseFragment(R.layout.fragment_note_detail) {
 
     private var curNote: Note? = null
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         subscribeToObservers()
@@ -29,6 +43,28 @@ class NoteDetailFragment : BaseFragment(R.layout.fragment_note_detail) {
             findNavController().navigate(
                 NoteDetailFragmentDirections.actionNoteDetailFragmentToAddEditNoteFragment(args.id)
             )
+        }
+
+        if (savedInstanceState != null) {
+            val addOwnerDialog = parentFragmentManager.findFragmentByTag(ADD_OWNER_DIALOG_TAG)
+                    as AddOwnerDialog?
+            addOwnerDialog?.setPositiveListener {
+                addOwnerToCurrentNote(it)
+            }
+        }
+    }
+
+    private fun showAddOwnerDialog() {
+        AddOwnerDialog().apply {
+            setPositiveListener {
+                addOwnerToCurrentNote(it)
+            }
+        }.show(parentFragmentManager, ADD_OWNER_DIALOG_TAG)
+    }
+
+    private fun addOwnerToCurrentNote(email: String) {
+        curNote?.let { note ->
+            viewModel.addOwnerToNote(email, note.id)
         }
     }
 
@@ -39,6 +75,25 @@ class NoteDetailFragment : BaseFragment(R.layout.fragment_note_detail) {
     }
 
     private fun subscribeToObservers() {
+
+        viewModel.addOwnerStatus.observe(viewLifecycleOwner, Observer {event ->
+            event?.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        addOwnerProgressBar.visibility = View.GONE
+                        showSnackbar(result.data ?: "Successfully added owner to note")
+                    }
+                    Status.ERROR -> {
+                        addOwnerProgressBar.visibility = View.GONE
+                        showSnackbar(result.message ?: "An unknow error occured")
+                    }
+                    Status.LOADING -> {
+                        addOwnerProgressBar.visibility = View.VISIBLE
+                    }
+                }
+            }
+        })
+
         viewModel.observeNoteById(args.id).observe(viewLifecycleOwner, Observer {
             it?.let { note ->
                 tvNoteTitle.text = note.title
@@ -46,6 +101,18 @@ class NoteDetailFragment : BaseFragment(R.layout.fragment_note_detail) {
                 curNote = note
             } ?: showSnackbar("Note not found")
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.note_detail_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.miAddOwner -> showAddOwnerDialog()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
